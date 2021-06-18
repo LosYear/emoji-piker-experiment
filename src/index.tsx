88f3ff1a35f044af6ae4f1c421566da62a7729ad
Yaroslav Losev <losevyaroslav@gmail.com>
2021-06-18 12:46:47 +0300

    Bootstrap project

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './style.scss';
+
+const element = document.querySelector('#app-root');
+
+ReactDOM.render(<div>Hello</div>, element);
